d78ea442626db22a35a6bb62104ab6aba447ea6c
Rohn Chatterjee <rohn.ch@gmail.com>
2024-03-06 17:02:57 +0530

    updated wallpaper

--- .config/luna_theme/colors-tty.sh
+++ .config/luna_theme/colors-tty.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 [ "${TERM:-none}" = "linux" ] && \
-    printf '%b' '\e]P0151010
-                 \e]P1d06446
-                 \e]P2d28c4d
-                 \e]P3d99a68
-                 \e]P4366ecc
-                 \e]P5ce3c82
-                 \e]P6477ed1
-                 \e]P7c3c3c5
-                 \e]P8888889
-                 \e]P9d06446
-                 \e]PAd28c4d
-                 \e]PBd99a68
-                 \e]PC366ecc
-                 \e]PDce3c82
-                 \e]PE477ed1
-                 \e]PFc3c3c5
+    printf '%b' '\e]P0101210
+                 \e]P180b62d
+                 \e]P2ca9232
+                 \e]P32a68aa
+                 \e]P44b8dd2
+                 \e]P5529bd3
+                 \e]P66a99da
+                 \e]P7c6d0db
+                 \e]P88a9199
+                 \e]P980b62d
+                 \e]PAca9232
+                 \e]PB2a68aa
+                 \e]PC4b8dd2
+                 \e]PD529bd3
+                 \e]PE6a99da
+                 \e]PFc6d0db
                  \ec'
